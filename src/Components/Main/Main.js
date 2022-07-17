bd588f2a14be7be4219f3bd556d44adf6b2603e9
khardy <khardy-official@gmail.com>
2022-07-17 16:34:14 +0300

    update

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -3,38 +3,48 @@ import { useSelector } from 'react-redux';
 
 function Main() {
     const projects = useSelector(state => state.cv.data.projects);
+    const profile = useSelector(state => state.cv.data.profile);
+    const books = useSelector(state => state.cv.data.books);
+    const courses = useSelector(state => state.cv.data.courses);
     return (
         <div className='Main'>
             <h1>Dmytro <span>Kyrychenko</span></h1>
             <h2>Front-end developer(<span>ReactJS</span>)</h2>
+            <h3>Profile</h3>
+            <p>{profile}</p>
             <h3>Projects</h3>
             <div>
                 <ul>
-                    <li> My first project was website layout based on PSD layout. It was made on "floats" that no one needs now, but a lot of effort was spent, including on "adaptive design". After that, I got acquainted with the power of "flex" and layout of sites ceased to be a problem.<br />
-                        <a href={projects.first}>{projects.first}</a>
+                    <li>This CV was made using the React library and Redux. Linear CSS animations were also used. You can see the work here: <br />
+                        <a href={projects.cv}>{projects.cv}</a>
                     </li>
                     <hr />
-                    <li>The 2nd project already covered the basic knowledge of "JS" and "jQuery" for using sliders and popups. <br />
-                        <a href={projects.polar}>{projects.polar}</a>
+                    <li>Also on GitHub you can see the work of my site  <br />
+                        <a href={projects.second}>{projects.second}</a>
                     </li>
                     <hr />
-                    <li>3rd is a mini game based on CSS animations. <br />
+                    <li>Mini game - based on CSS animations. <br />
                         <a href={projects.dino}>{projects.dino}</a>
                     </li>
                     <hr />
-                    <p>After "JS", I started to get acquainted with "JSX" and the "React" library</p>
-                    <hr />
-                    <li>4th mini-project (shop cart) - which shows the logic of working with the state and the "Redux" library. <br />
-                        <a href={projects.goods}>{projects.goods}</a>
+                    <li>Shop cart - which shows the logic of working with the state and the "Redux" library. <br />
+                        <a href={projects.cart}>{projects.cart}</a>
                     </li>
                     <hr />
-                    <li> The 5th project already affects the "backend" and the logic of communicating with a remote server has been implemented. <br />
+                    <li> And the most complex project is a Node.js messaging application interacting with a MySQL database through the sequenceize module. <br />
                         <a href={projects.notes}>{projects.notes}</a>
                     </li>
                     <hr />
-                    <li>Well, the 6th one is "react-cv" with using Redux. <br />
-                        <a href={projects.cv}>{projects.cv}</a>
-                    </li>
+                </ul>
+            </div>
+            <h3>Courses</h3>
+            <div style={{ display: "flex", justifyContent: "space-around", flexWrap: "wrap"}}>
+                {courses.map(item => <img style={{ display: "block", width: "148px", marginTop: "5px"}} src={item} alt="logo" key={item}></img>)}
+            </div>
+            <h3>Books</h3>
+            <div>
+                <ul>
+                    {books.map(item => <li key={item}>{item}</li>)}
                 </ul>
             </div>
         </div>
--- src/Components/Sidebar/Social/Social.js
+++ src/Components/Sidebar/Social/Social.js
@@ -9,9 +9,10 @@ function Social() {
             <div><img className='main' width="264px"  src={data.img} alt="main"></img></div>
             <div><a href="tel:+380679718191"><img className='icons' width="36px" src="./icons/tel.svg" alt="tel" /> {data.tel}</a></div>
             <div><a href="mailto:khardy.official@gmail.com"><img className='icons' width="36px" src="./icons/mail.svg" alt="mail" /> {data.gmail}</a></div>
-            <div><a href="https://join.skype.com/invite/DRJld7GE4cBW"><img className='icons' width="36px" src="./icons/skype.svg" alt="skype" /> Kyrychenko Dmytro</a></div>
-            <div><a href="https://www.linkedin.com/in/dmytro-kyrychenko-b33b54243/"><img className='icons' width="36px" src="./icons/linkedin.svg" alt="linkedin" /> linkedin</a></div>
+            <div><a href="https://join.skype.com/invite/DRJld7GE4cBW"><img className='icons' width="36px" src="./icons/skype.svg" alt="skype" /> {data.skype}</a></div>
+            <div><a href="https://www.linkedin.com/in/dmytro-kyrychenko-b33b54243/"><img className='icons' width="36px" src="./icons/linkedin.svg" alt="linkedin" /> {data.linkedin}</a></div>
             <div><a href="https://github.com/khardy-official"><img className='icons' width="36px" src="./icons/github.svg" alt="github" /> khardy-official</a></div>
+            <div><a href="https://www.codewars.com/users/khardy-official/badges/large"><img className='icons' width="36px" src="./icons/codewars.svg" alt="github" /> Codewars</a></div>
             <div><img className='icons' width="36px" src="./icons/location.svg" alt="location" /> {data.address}</div>
             <hr />
         </div>

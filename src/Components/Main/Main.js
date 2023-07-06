79bbc309413f1a635a4f8f371ad5497b67728a2d
khardy <khardy.official@gmail.com>
2023-07-06 15:30:01 +0300

    added 2 projects

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -14,22 +14,36 @@ function Main() {
             <p>{profile}</p>
             <h3>Projects</h3>
             <div className='projects'>
+                <div className="snake">
+                    <h2><a href={projects.snake} target="_blank" rel="noreferrer">Snake</a></h2>
+                    <ul>
+                        <li>Project description: The Snake Game includes storing the best result in the database.</li>
+                        <li>Technology stack: PERN - PostgreSQL, Express, React, Node.</li>
+                    </ul>
+                </div>
+                <div className="delivery">
+                    <h2><a href={projects.delivery} target="_blank" rel="noreferrer">Delivery</a></h2>
+                    <ul>
+                        <li>Project description: The selection of stores and ordering of goods.</li>
+                        <li>Technology stack: MERN - MongoDB, Express, React, Node. </li>
+                    </ul>
+                </div>
                 <div className="pizza">
-                    <h2><a href={projects.pizza}>Pizza</a></h2>
+                    <h2><a href={projects.pizza} target="_blank" rel="noreferrer">Pizza</a></h2>
                     <ul>
                         <li>Project description: Pizza online store.</li>
                         <li>Technology stack: React, TS, Redux Toolkit, Scss Modules, Responsive design. </li>
                     </ul>
                 </div>
                 <div className="pets-paw">
-                    <h2><a href={projects.pets}> </a></h2>
+                    <h2><a href={projects.pets} target="_blank" rel="noreferrer"> </a></h2>
                     <ul>
                         <li>Project description: Cat gallery app.</li>
                         <li>Technology stack: React, Redux Toolkit,Rest API, Scss, Axios, Figma. </li>
                     </ul>
                 </div>
                 <div className='chat'>
-                    <h2><a href={projects.chat}>Chat</a></h2>
+                    <h2><a href={projects.chat} target="_blank" rel="noreferrer">Chat</a></h2>
                     <ul>
                         <li>Project description: Chat app with automatically generated response.</li>
                         <li>Technology stack: React, Redux Toolkit,Rest API, Scss, Axios, Local Storage</li>
@@ -38,8 +52,8 @@ function Main() {
             </div>
             <h3>Courses</h3>
             <div style={{ display: "flex", justifyContent: "space-evenly", flexWrap: "wrap", gap: "20px" }}>
-                {courses.map(item => <div style={{ display: "block", flex: "0 1 240px", marginTop: "5px" }} key={item}><img src={item} alt="logo" 
-                style={{ display: "block", width: "100%", height: "auto"}}
+                {courses.map(item => <div style={{ display: "block", flex: "0 1 240px", marginTop: "5px" }} key={item}><img src={item} alt="logo"
+                    style={{ display: "block", width: "100%", height: "auto" }}
                 ></img></div>)}
             </div>
         </div>
--- src/Components/Sidebar/Social/Social.js
+++ src/Components/Sidebar/Social/Social.js
@@ -3,16 +3,17 @@ import './Social.css';
 
 function Social() {
     const data = useSelector(state => state.cv.data.social);
-    
+
     return (
         <div className="Social">
             <div><img className='main' src={data.img} alt="main"></img></div>
             <div><a href="tel:+380679718191"><img className='icons' width="36px" src="./icons/tel.svg" alt="tel" /> {data.tel}</a></div>
-            <div><a href="mailto:khardy.official@gmail.com"><img className='icons' width="36px" src="./icons/mail.svg" alt="mail" /> {data.gmail}</a></div>
-            <div><a href="https://join.skype.com/invite/DRJld7GE4cBW"><img className='icons' width="36px" src="./icons/skype.svg" alt="skype" /> {data.skype}</a></div>
-            <div><a href="https://www.linkedin.com/in/dmytro-kyrychenko-b33b54243/"><img className='icons' width="36px" src="./icons/linkedin.svg" alt="linkedin" /> {data.linkedin}</a></div>
-            <div><a href="https://github.com/khardy-official"><img className='icons' width="36px" src="./icons/github.svg" alt="github" /> khardy-official</a></div>
-            <div><a href="https://www.codewars.com/users/khardy-official/badges/large"><img className='icons' width="36px" src="./icons/codewars.svg" alt="github" /> Codewars</a></div>
+            <div><a href="https://t.me/khardy209" target="_blank" rel="noreferrer"><img className='icons' width="36px" src="./icons/telegram.svg" alt="telegram"/> {data.telegram}</a></div>
+            <div><a href="mailto:khardy.official@gmail.com" target="_blank" rel="noreferrer"><img className='icons' width="36px" src="./icons/mail.svg" alt="mail" target="_blank" /> {data.gmail}</a></div>
+            <div><a href="https://join.skype.com/invite/DRJld7GE4cBW" target="_blank" rel="noreferrer"><img className='icons' width="36px" src="./icons/skype.svg" alt="skype" /> {data.skype}</a></div>
+            <div><a href="https://www.linkedin.com/in/dmytro-kyrychenko-b33b54243/" target="_blank" rel="noreferrer"><img className='icons' width="36px" src="./icons/linkedin.svg" alt="linkedin" /> {data.linkedin}</a></div>
+            <div><a href="https://github.com/khardy-official" target="_blank" rel="noreferrer"><img className='icons' width="36px" src="./icons/github.svg" alt="github" /> khardy-official</a></div>
+            <div><a href="https://www.codewars.com/users/khardy-official/badges/large" target="_blank" rel="noreferrer"><img className='icons' width="36px" src="./icons/codewars.svg" alt="github" /> Codewars</a></div>
             <div><img className='icons' width="36px" src="./icons/location.svg" alt="location" /> {data.address}</div>
             <hr />
         </div>

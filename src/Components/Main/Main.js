c6fd700638b94f20e912bfcab4671618a8e219ea
khardy <khardy.official@gmail.com>
2023-08-09 23:18:46 +0300

    updated

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -12,27 +12,13 @@ function Main() {
             <h2>Full Stack Developer  (<span>React / NodeJS</span>)</h2>
             <h3>Profile</h3>
             <p>{profile}</p>
-            <h3>Projects</h3>
             <div className='projects'>
-            <div className="organick">
-                    <h2><a href={projects.organick} target="_blank" rel="noreferrer">Organick</a></h2>
+                <h3>Front-end projects:</h3>
+                <div className="pets-paw">
+                    <h2><a href={projects.pets} target="_blank" rel="noreferrer"> </a></h2>
                     <ul>
-                        <li>Project description: Organic is a grocery e-commerce website where you can order a variety of fresh products to suit every taste.</li>
-                        <li>Technology stack: (MERN - MongoDB, Express, React, Node), Responsive design, Redux Toolkit, Rest API, Scss, Axios, Figma</li>
-                    </ul>
-                </div>
-                <div className="snake">
-                    <h2><a href={projects.snake} target="_blank" rel="noreferrer">Snake</a></h2>
-                    <ul>
-                        <li>Project description: The Snake Game includes storing the best result in the database.</li>
-                        <li>Technology stack: PERN - PostgreSQL, Express, React, Node.</li>
-                    </ul>
-                </div>
-                <div className="delivery">
-                    <h2><a href={projects.delivery} target="_blank" rel="noreferrer">Delivery</a></h2>
-                    <ul>
-                        <li>Project description: The selection of stores and ordering of goods.</li>
-                        <li>Technology stack: MERN - MongoDB, Express, React, Node. </li>
+                        <li>Project description: Cat gallery app.</li>
+                        <li>Technology stack: React, Redux Toolkit,Rest API, Scss, Axios, Figma. </li>
                     </ul>
                 </div>
                 <div className="pizza">
@@ -42,18 +28,33 @@ function Main() {
                         <li>Technology stack: React, TS, Redux Toolkit, Scss Modules, Responsive design. </li>
                     </ul>
                 </div>
-                <div className="pets-paw">
-                    <h2><a href={projects.pets} target="_blank" rel="noreferrer"> </a></h2>
-                    <ul>
-                        <li>Project description: Cat gallery app.</li>
-                        <li>Technology stack: React, Redux Toolkit,Rest API, Scss, Axios, Figma. </li>
-                    </ul>
-                </div>
                 <div className='chat'>
                     <h2><a href={projects.chat} target="_blank" rel="noreferrer">Chat</a></h2>
                     <ul>
                         <li>Project description: Chat app with automatically generated response.</li>
                         <li>Technology stack: React, Redux Toolkit,Rest API, Scss, Axios, Local Storage</li>
+                    </ul>
+                </div>
+                <h3>Full-stack projects:</h3>
+                <div className="delivery">
+                    <h2><a href={projects.delivery} target="_blank" rel="noreferrer">Delivery</a></h2>
+                    <ul>
+                        <li>Project description: The selection of stores and ordering of goods.</li>
+                        <li>Technology stack: MERN - MongoDB, Express, React, Node. </li>
+                    </ul>
+                </div>
+                <div className="snake">
+                    <h2><a href={projects.snake} target="_blank" rel="noreferrer">Snake</a></h2>
+                    <ul>
+                        <li>Project description: The Snake Game includes storing the best result in the database.</li>
+                        <li>Technology stack: PERN - PostgreSQL, Express, React, Node.</li>
+                    </ul>
+                </div>
+                <div className="organick">
+                    <h2><a href={projects.organick} target="_blank" rel="noreferrer">Organick</a></h2>
+                    <ul>
+                        <li>Project description: Organic is a grocery e-commerce website where you can order a variety of fresh products to suit every taste.</li>
+                        <li>Technology stack: (MERN - MongoDB, Express, React, Node), Responsive design, Redux Toolkit, Rest API, Scss, Axios, Figma</li>
                     </ul>
                 </div>
             </div>
--- src/Components/Sidebar/Skills/Skills.js
+++ src/Components/Sidebar/Skills/Skills.js
@@ -1,9 +1,7 @@
 import { useSelector } from 'react-redux';
 
 function Skills() {
-    const skills = useSelector(state => state.cv.data.skills);
-    const education = useSelector(state => state.cv.data.education);
-        const books = useSelector(state => state.cv.data.books);
+    const {skills, education, books, certificateLinks} = useSelector(state => state.cv.data)
     return (
         <div className="Skills">
             <h2>Skills</h2>
@@ -12,12 +10,18 @@ function Skills() {
             </ul>
             <h2>Education</h2>
             <div>{education}</div>
-            <h2>Books</h2>
+            <h2>Certificate links</h2>
+            <ul>
+            {Object.keys(certificateLinks).map((item) => (
+                <li key={item}><a href={certificateLinks[item]} target="_blank" rel="noreferrer">{item}</a></li>
+            ))}
+            </ul>
+            {/* <h2>Books</h2>
             <div>
                 <ul>
-                    {books.map(item => <li style={{marginBottom: "20px"}} key={item}>{item}</li>)}
+                    {books.map(item => <li style={{ marginBottom: "20px" }} key={item}>{item}</li>)}
                 </ul>
-            </div>
+            </div> */}
         </div>
     )
 }
